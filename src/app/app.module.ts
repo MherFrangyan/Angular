6124d9abfbbd44739eac4a12163e3db331a093fe
MherFrangyan <mherf@mail.ru>
2020-06-17 18:30:27 +0400

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    Demo2Component
   ],
   imports: [
     BrowserModule
